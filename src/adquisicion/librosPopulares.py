3c6555f8b0a4183e53c2d7761c7957fd316bd85b
JaviDeMartino <115741804+JaviDeMartino@users.noreply.github.com>
2024-02-26 18:34:56 +0100

    sobran importaciones

--- src/adquisicion/librosPopulares.py
+++ src/adquisicion/librosPopulares.py
@@ -8,8 +8,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import ElementClickInterceptedException
 
 # Url base de GoodReads
 URL_BASE = "https://www.goodreads.com/book/popular_by_date/"
