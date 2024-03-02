5bf4e1bae9ff227a70aa116199d66b83ad08d33f
JaviDeMartino <115741804+JaviDeMartino@users.noreply.github.com>
2024-03-03 00:09:48 +0100

    googleTrends review
    nueva función para preparar las fechas y una alternativa que sigue dando el error 429 pero que puede funcionar

--- src/adquisicion/googleTrends.py
+++ src/adquisicion/googleTrends.py
@@ -10,6 +10,45 @@ pytrends = TrendReq(hl='en-US', tz=360)
 CATEGORIA_TODAS = 0
 CATEGORIA_LIBROS = 22
 
+def gestionarFechasParaTrends(df):
+    """Filtra y prepara las fechas para que puedan ser analizadas por GoogleTrends"""
+
+    DIAS = 20
+    
+    df1 = df[df["potencialBS"] == 1]
+
+    # Convertir las columnas a tipo de dato de fecha
+    df1['Date'] = pd.to_datetime(df1['Date'], errors='coerce')
+    df1['DatePublished'] = pd.to_datetime(df1['DatePublished'], errors='coerce')
+
+    # Encontrar las fechas que están fuera del rango permitido
+    out_of_bounds_dates = df1[(df1['Date'].isnull()) | (df1['DatePublished'].isnull())]
+
+    df1 = df1.drop(out_of_bounds_dates.index)
+
+    df1['Date'] = pd.to_datetime(df1['Date'])
+    df1['DatePublished'] = pd.to_datetime(df1['DatePublished'])
+    df1['DaysDifference'] = (df1['Date'] - df1['DatePublished']).dt.days
+    
+    df2 = df[df["potencialBS"] == 0]
+    df2['DatePublished'] = pd.to_datetime(df2['DatePublished'], errors='coerce')
+
+    out_of_bounds_dates = df2[(df2['DatePublished'].isnull())]
+
+    # Eliminar las filas con fechas fuera de rango en df2
+    df2 = df2.drop(out_of_bounds_dates.index)
+
+    # Le sumamos la mediana para calcular la columna Date
+    df2['Date'] = df2['DatePublished'] + pd.Timedelta(days= DIAS)
+
+    # Calcular la diferencia en días entre las fechas en df2
+    df2['DaysDifference'] = (df2['Date'] - df2['DatePublished']).dt.days
+    
+    df = pd.concat([df1, df2])
+    df = df[df['DaysDifference'] > 0]
+    
+    return df
+
 def restarDias(fecha_str, dias):
     """Resta el número de días dado a una fecha de tipo str"""
     
@@ -76,3 +115,5 @@ def getTrends(df_bestsellers):
     df_bestsellers["AuthorInterest1Y"] = interestAuthors
 
     return df_bestsellers
+
+
