d637231d948f5cfb0cfa545932c920a32c15e3cf
María Romero Huertas <mrhy2003@gmail.com>
2024-03-03 16:11:00 +0100

    Reviews.py funciona. Actualiza archivos_info.txt

--- src/adquisicion/goodreadsReviews.py
+++ src/adquisicion/goodreadsReviews.py
@@ -4,65 +4,116 @@ import asyncio
 import time
 from playwright.async_api import async_playwright
 import re
+import pandas as pd
+from datetime import datetime, timedelta
+from dateutil import parser
 
-browser_url = f'https://www.goodreads.com/book/show/11588.The_Shining?from_search=true&from_srp=true&qid=taAZMILcU8&rank=1'
 
-
+# leer el df de libros, para cada fila hacer un link con el titulo, hacer todo eso y añadir los valores a las columnas. tener en cuenta la fecha para q la review sea valida y guardar todo al final en un csv
 async def main():
-    async with async_playwright() as pw:
-        print('start');
-        browser = await pw.chromium.launch_persistent_context(
-                "",
-                headless=False,
-                args=[f"--disable-extensions",]
-                )
-        print('launched');
-        page = await browser.new_page()
-        await page.goto(browser_url, timeout=120000)
-        print('loaded')
+    df = pd.read_csv('/Users/maria/Downloads/TEST_LIMPIEZA.csv')
+    
+    for index, row in df.loc[:5, :].iterrows():
+        titulo = row['Title']
+        browser_url = row['url']
+        
+        print(browser_url)
+        
+        async with async_playwright() as pw:
+            print('start');
+            browser = await pw.chromium.launch_persistent_context(
+                    "",
+                    headless=False,
+                    args=[f"--disable-extensions",]
+                    )
+            print('launched');
+            page = await browser.new_page()
+            await page.goto(browser_url, timeout=120000)
+            print('loaded')
 
-        el = page.get_by_text('More reviews and ratings')
-        print(el)
-        await el.dispatch_event('click')
-
-
-        await page.locator('xpath=//html/body/div[3]/div/div[1]/div/div/button').click()
-        print("popup closed")
-
-
-        await page.locator('xpath=//*[@id="__next"]/div[2]/main/div[1]/div[2]/div[4]/div[1]/div[2]/div/button').click()
-        print('Filters clicked')
-
-        el = page.get_by_text('Oldest first')
-        print(el)
-        await el.dispatch_event('click')
-        print('oldest first clicked')
-
-        el = page.get_by_text('Apply')
-        print(el)
-        await el.dispatch_event('click')
-        print('apply clicked')
-
-        # paginate
-        for idx in range(10):
-            print(f"Page {idx}")
-            await page.locator('xpath=//*[@id="__next"]/div[2]/main/div[1]/div[2]/div[5]/div[4]/div/button').click()
+            el = page.get_by_text('More reviews and ratings')
+            print(el)
+            await el.dispatch_event('click')
 
 
-        # get all ratings
-        ratings = await page.locator('section.ReviewCard__row > div.ShelfStatus').all()
+            await page.locator('xpath=//html/body/div[3]/div/div[1]/div/div/button').click()
+            print("popup closed")
 
-        for item in ratings:
-            rs = await item.locator('span').all()
-            if rs:
-                stars_text = await rs[0].get_attribute("aria-label")
-                pattern = r'\b\d+\b'
-                numbers = re.findall(pattern, stars_text)
-                if numbers:
-                    rating = int(numbers[0])
-                    print(f"Rating: {rating}")
 
-        time.sleep(100)
-        await browser.close()
+            await page.locator('xpath=//*[@id="__next"]/div[2]/main/div[1]/div[2]/div[4]/div[1]/div[2]/div/button').click()
+            print('Filters clicked')
+
+            el = page.get_by_text('Oldest first')
+            print(el)
+            await el.dispatch_event('click')
+            print('oldest first clicked')
+
+            el = page.locator('body > div.Overlay.Overlay--floating > div > div.Overlay__actions > div:nth-child(2) > button > span')
+            print(el)
+            await el.dispatch_event('click')
+            print('apply clicked')
+
+            # paginate
+            NUM_PAGES = 2
+            for idx in range(NUM_PAGES):
+                try:
+                    print(f"Page {idx}")
+                    await page.locator('xpath=//*[@id="__next"]/div[2]/main/div[1]/div[2]/div[5]/div[4]/div/button').click()
+                except Exception as e:
+                    print(e)
+                    break
+
+            # get all ratings
+            ratings = await page.locator('section.ReviewCard__row').all()
+            
+            num_ratings = 0
+            sum_ratings = 0
+            
+            for item in ratings:
+                
+                rs = await item.locator('span').all()
+                
+                
+                if rs:
+                    stars_text = await rs[0].get_attribute("aria-label")
+                    if stars_text:
+                        pattern = r'\b\d+\b'
+                        numbers = re.findall(pattern, stars_text)
+                        
+                        date_text = await rs[6].inner_text()
+                        
+                        if date_text:
+                            try:
+                                review_date = datetime.strptime(date_text, '%B %d, %Y')
+                                limit_date = datetime.strptime(row['Date'], '%Y-%m-%d')
+                                
+                                if review_date <= limit_date and numbers:
+
+                                    rating = int(numbers[0])
+                                    num_ratings += 1
+                                    sum_ratings += rating
+                                    
+                                        
+                            except ValueError:
+                                print(f"Error parsing date: {date_text}")
+                                continue
+                    else:
+                        print(f"stars_text is {stars_text}. Skipping this rating.")
+                   
+            if num_ratings == 0:
+                mean = None
+            else:
+                mean = sum_ratings / num_ratings
+                
+            print(mean)
+            df.at[index, 'Rating'] = round(mean, 2)
+            df.at[index, 'numRatings'] = num_ratings
+            
+            print('sleep5')
+            await asyncio.sleep(5)
+            await browser.close()
+            print('Browser closed')
+            
+    df.to_csv(f"conRatings.csv")
 
 asyncio.run(main())
