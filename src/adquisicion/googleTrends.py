886a105c6697500c6299b7396201a8dacba7061f
JaviDeMartino <115741804+JaviDeMartino@users.noreply.github.com>
2024-03-03 22:55:29 +0100

    ahora funciona :D
    accediendo a los trends mediante keywords avanzadas parece que gestiona mejor el error 429

--- src/adquisicion/googleTrends.py
+++ src/adquisicion/googleTrends.py
@@ -1,11 +1,8 @@
+import pandas as pd
 from datetime import datetime, timedelta
 from pytrends.request import TrendReq
 import time
-import re
-
-# hl -> hosting language
-# tz -> timezone (360 = USA)
-pytrends = TrendReq(hl='en-US', tz=360)
+import pytrends
 
 CATEGORIA_TODAS = 0
 CATEGORIA_LIBROS = 22
@@ -49,71 +46,78 @@ def gestionarFechasParaTrends(df):
     
     return df
 
-def restarDias(fecha_str, dias):
-    """Resta el número de días dado a una fecha de tipo str"""
-    
-    fecha_obj = datetime.strptime(fecha_str, '%Y-%m-%d')
-    fecha_resultante = fecha_obj - timedelta(days=dias)
-    nueva_fecha_str = fecha_resultante.strftime('%Y-%m-%d')
-
-    return nueva_fecha_str
+CATEGORIA_LIBROS = 22
 
 def getInterestOverTime(keywords, categoria, tf):
     """Devuelve datos relacionados con el interés a lo largo del tiempo
     de la lista de keywords introducidas en una categoría y un timeframe dados"""
-    
-    pytrends.build_payload(keywords, cat=categoria, timeframe=tf) 
-    data = pytrends.interest_over_time() 
-    data = data.reset_index()
-    
-    return data
+    try:
+        pytrends.build_payload(keywords, cat=categoria, timeframe=tf) 
+        data = pytrends.interest_over_time() 
+        data = data.reset_index()
+        return data
+    except Exception as e:
+        print(e)
+    return pd.DataFrame()
 
-def getTrends(df_bestsellers):
-    """Dado un dataframe de bestsellers, obtiene el interés para cada libro y autor"""
+def getAdvancedKeyword(kw):
+    """Obtiene la keyword codificada dado un título"""
+
+    suggestions = pytrends.suggestions(keyword=kw)
+    book = next((item for item in suggestions if 'Book' in item['type']), None)
+    if book != None:
+        return book['mid']
+    return kw
+
+
+def getTrends(df):
+    """Dado un dataframe de bestsellers, obtiene el interés para cada libro en el periodo
+    de un mes"""
     
     interestBooks = []
-    interestAuthors = []
 
-    count = 0
+    # hl -> hosting language
+    # tz -> timezone (360 = USA)
+    pytrends = TrendReq(hl='en-US', tz=360)
+
+    # Nos aseguramos de que la columna Date sea una fecha
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    count429 = 0
     totalCount = 0
     # Recorremos todos los bestsellers
-    for i, row in df_bestsellers.iterrows():
+    for i, row in df.iterrows():
 
-        book = row.Book
-        authors = re.split(r'\s+and\s+|\s+with\s+', row.Author)
+        book = row.Title
 
         # Creamos las listas de palabras clave
         keywordsBook = [book]
-        keywordsAuthor = authors
 
         # Generamos los timeframes
-        date = "{:04d}-{:02d}-{:02d}".format(row.Year, row.Month, row.Day)
-        minusMonth = restarDias(date, 30)
-        minusYear = restarDias(date, 30)
-        tfM = minusMonth + " " + date
-        tfY = minusYear + " " + date
+        date = row.Date
+        minusMonth = date - timedelta(days=30)
+        tfM = minusMonth.strftime("%Y-%m-%d") + " " + date.strftime("%Y-%m-%d")
 
         # Hacemos la request para el título
         data = getInterestOverTime(keywordsBook, CATEGORIA_LIBROS, tfM)
-        interestBooks.append(sum(data[book]))
+        try:
 
-        # Hacemos la request para el autor/es
-        data = getInterestOverTime(keywordsAuthor, CATEGORIA_TODAS, tfY)
-        interestAuthors.append(sum([sum(data[a]) for a in authors]))
-
+            suma = sum(data[book])
+            interestBooks.append(suma)
+            
+        except Exception as e:
+            interestBooks.append(0)
+            count429 += 1
+        
+        if count429 == 10:
+            count429 = 0
+            time.sleep(60)
 
         print(totalCount)
-        count += 1
         totalCount += 1
+        time.sleep(1)
 
-        # Congelamos para que Google no de response code 439 (demasiadas requests)
-        if count == 30:
-            time.sleep(2)
-            count = 0
+    df["BookInterest1M"] = interestBooks
 
-    df_bestsellers["BookInterest1M"] = interestBooks
-    df_bestsellers["AuthorInterest1Y"] = interestAuthors
-
-    return df_bestsellers
-
+    return df
 
